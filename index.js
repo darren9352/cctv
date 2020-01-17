72a5a8072e8a371a02c2b8b79bf5d40e81197cc8
darren9352 <darren9352@gmail.com>
2020-01-18 04:07:39 +0900

    develope face recog image to video

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+require('dotenv').config();
 
 const FPS = 30;
 
@@ -15,10 +16,19 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-
 setInterval(()=>{
     const frame = wCap.read();
     const image = cv.imencode('.jpg', frame).toString('base64');
+    const request = require('request');
+    const api_rul = 'https://naveropenapi.apigw.ntruss.com/vision/v1/face';
+    const _formData = {
+        image: 'image',
+        image: cv.imencode('.jpg', frame)
+    };
+    request.post({url:api_rul, formData:_formData,
+    headers:{'X-NCP-APIGW-API-KEY-ID': process.env.CLIENT_ID, 'X-NCP-APIGW-API-KEY': process.env.CLIENT_SECRET}}, function(err, httpResponse, body){
+        console.log(body);
+    })
     io.emit('image', image);
 }, 1000 / FPS)
 
