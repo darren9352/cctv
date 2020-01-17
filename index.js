c83813a66705a6f7d9c86a56c2d8a733480bfe51
darren9352 <darren9352@gmail.com>
2020-01-18 02:57:41 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const cv = require('opencv4nodejs');
+const express = require('express');
+const path = require('path');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const FPS = 30;
+
+const wCap = new cv.VideoCapture(0);
+wCap.set(cv.CAP_PROP_FRAME_WIDTH, 300);
+wCap.set(cv.CAP_PROP_FRAME_HEIGHT, 300);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+setInterval(()=>{
+    const frame = wCap.read();
+    const image = cv.imencode('.jpg', frame).toString('base64');
+    io.emit('image', image);
+}, 1000 / FPS)
+
+server.listen(3000);
